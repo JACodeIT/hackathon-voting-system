4c61776f4011a402787ba37d8488b3f854a4d11e
JA Cereno <cereno.joseangelito.n@gmail.com>
2023-11-05 02:05:09 +0800

    chore: added db create statement and local env for postman

---
+++ devtools/sql_scripts/hvd_create_stmt.sql
@@ -0,0 +1,7 @@
+-- =========================================
+-- MySQL v8
+-- Database: hackathon_voting_daedalus
+-- =========================================
+DROP SCHEMA IF EXISTS `hackathon_voting_daedalus`;
+CREATE SCHEMA IF NOT EXISTS `hackathon_voting_daedalus`;
+USE `hackathon_voting_daedalus`;
