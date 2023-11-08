03d1bde41bded55d45a399aeaa835e739dd82c85
JA Cereno <joseangelito.cereno@gmail.com>
2023-11-09 03:35:20 +0800

    feat: added Community & Public Votes Controller and Model | updated Judges Scoreboard Model

--- backend/app/Models/Judges_Scoreboard.php
+++ backend/app/Models/Judges_Scoreboard.php
@@ -18,4 +18,26 @@ class Judges_Scoreboard extends Model
         'squad_id',
         'score',
     ];
+
+    /**
+     * Get the user associated with the Judges_Scoreboard
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function judge()
+    {
+        return $this->hasOne(Member::class, 'id', 'event_judge');
+    }
+
+    /**
+     * Get all of the criteria for the Judges_Scoreboard
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function criteria()
+    {
+        return $this->hasMany(Criteria::class, 'id', 'event_criteria');
+    }
+
+
 }
