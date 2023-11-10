5640e37b753827468d864747effcb87b8863ecf7
JA Cereno <joseangelito.cereno@gmail.com>
2023-11-11 05:03:09 +0800

    fix for Multiple primary key defined

--- backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->primary();
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
