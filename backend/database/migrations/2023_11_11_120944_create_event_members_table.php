05b22fa9ae8da12d0e2ac5cc31bf290a563a37ed
JA Cereno <joseangelito.cereno@gmail.com>
2023-11-11 20:42:43 +0800

    feat: added migration table for event participants

---
+++ backend/database/migrations/2023_11_11_120944_create_event_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_participants', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('event_id')->unsigned();
+            $table->bigInteger('member_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('member_id')->references('id')->on('members');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_participants');
+    }
+};
